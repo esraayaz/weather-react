172202d812bf020bb27854abd8ba4f9d655fe722
esraayaz <essayaz@hotmail.com>
2022-05-23 00:48:59 +0200

    Forecast and weather info add ons are made

---
+++ src/WeatherForecast.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+import Axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay.js";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "df1e7d566653ff630e34dbe6521e4121";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${longitude}&lon=${latitude}&appid=${apiKey}&units=metric`;
+
+    Axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
